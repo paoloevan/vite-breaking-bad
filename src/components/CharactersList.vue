09b06bb09f358e127c7b207d7bd6f1cc64a771d3
paoloevan <paoloev1900@gmail.com>
2022-11-14 15:11:25 +0100

    add CharactersItem component

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,70 +1,18 @@
 <script>
+import CharactersItem from './CharactersItem.vue'
 export default {
-    name: 'CharactersList'
+    name: 'CharactersList',
+    components: {
+        CharactersItem
+    }
 }
 </script>
 
 <template>
     <h6>Found 63 characters</h6>
 
-    <div class="container">
-        <div class="row row-cols-5">
-            <div class="col">
-                <div class="my_card">
-                    <img src="https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg"
-                        alt="">
-                    <div class="name">Lorem, ipsum.</div>
-                    <div class="series">Lorem, dolor.</div>
-                    <div class="state">Presumed dead</div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="my_card">
-                    <img src="https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg"
-                        alt="">
-                    <div class="name">Lorem, ipsum.</div>
-                    <div class="series">Lorem, dolor.</div>
-                    <div class="state">Presumed dead</div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="my_card">
-                    <img src="https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg"
-                        alt="">
-                    <div class="name">Lorem, ipsum.</div>
-                    <div class="series">Lorem, dolor.</div>
-                    <div class="state">Presumed dead</div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="my_card">
-                    <img src="https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg"
-                        alt="">
-                    <div class="name">Lorem, ipsum.</div>
-                    <div class="series">Lorem, dolor.</div>
-                    <div class="state">Presumed dead</div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="my_card">
-                    <img src="https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg"
-                        alt="">
-                    <div class="name">Lorem, ipsum.</div>
-                    <div class="series">Lorem, dolor.</div>
-                    <div class="state">Presumed dead</div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="my_card">
-                    <img src="https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg"
-                        alt="">
-                    <div class="name">Lorem, ipsum.</div>
-                    <div class="series">Lorem, dolor.</div>
-                    <div class="state">Presumed dead</div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <CharactersItem />
+
 </template>
 
 <style lang="scss" scoped>
@@ -75,27 +23,5 @@ h6 {
     padding: 1rem;
     margin-bottom: 1rem;
     background-color: rgb(30, 30, 30);
-}
-
-.col {
-    margin-bottom: 0.5rem;
-
-    .my_card {
-        padding: 1rem;
-        background-color: rgb(0, 42, 52);
-        text-align: center;
-
-        .name {
-            color: white;
-            text-transform: uppercase;
-            font-weight: 600;
-        }
-
-        .series,
-        .state {
-            color: gray;
-            margin-top: 0.25rem;
-        }
-    }
 }
 </style>
